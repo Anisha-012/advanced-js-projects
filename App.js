67c468e1834efbf91d54b82a3e5c5c41fec14fcf
Anisha Das <74406679+Anisha-012@users.noreply.github.com>
2023-03-10 20:45:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import Game from './components/Game';
+import Square from './components/Square';
+import Board from './components/Board';
+const App =() => <Game />;
+  export default App;
